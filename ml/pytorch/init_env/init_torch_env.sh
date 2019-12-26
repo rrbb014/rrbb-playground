912a4ccd0db7eb744b3dbbde2796494aebc400e6
rrbb014 <gutssoul1@gmail.com>
2019-12-27 00:07:35 +0900

    ADDED: required libs and configure shell

--- ml/pytorch/init_env/init_torch_env.sh
+++ ml/pytorch/init_env/init_torch_env.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 PYVERSION=3.7.6
 
+
 # install pyenv
 
-sudo apt update
-sudo apt dist-upgrade 
+#sudo apt update
+#sudo apt dist-upgrade 
 
-sudo apt install git curl
+#sudo apt install git curl
 
 cd $HOME
 
+echo =========================
+echo install required packages
+echo =========================
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+    xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
-exec $SHELL
+echo export PATH="$HOME/.pyenv/bin:$PATH" >> $HOME/.zshrc
+echo 'eval "$(pyenv init -)"' >> $HOME/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.zshrc
 
 pyenv install $PYVERSION
 pyenv virtualenv $PYVERSION torch
 
-echo "Check pyenv versions"
+pyenv activate torch
+pip3 install -r requirements.txt --user
