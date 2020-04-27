e2f9609089257658b39558a50a70e538580e8fd8
rrbb014 <gutssoul1@gmail.com>
2020-04-27 22:01:23 +0900

    update 2020-04-27

--- golang/003-function/paint.go
+++ golang/003-function/paint.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 )
 
-func paintNeeded(width float64, height float64) {
+var meterPerLiter float64
+
+func paintNeeded(width float64, height float64) float64 {
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+	//fmt.Printf("%.2f liters needed\n", area/10.0)
+	return area / meterPerLiter
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
+	meterPerLiter = 10.0
+	fmt.Printf("%.2f", paintNeeded(4.2, 3.0))
+	//fmt.Println(area)    // area's variable scope는 paintNeeded 함수 내부라서 에러
 	paintNeeded(5.2, 3.5)
 	paintNeeded(5.0, 3.3)
 }
